13225c5464694a90abca0504d0e054c0670c9c9a
Aakash Thatte <aathatte2002@gmail.com>
2022-06-04 22:18:42 +0530

    Add code to implement age functionality, not yet completed

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.18
 require (
 	github.com/TheTitanrain/w32 v0.0.0-20180517000239-4f5cfb03fabf // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/shomali11/commander v0.0.0-20191122162317-51bc574c29ba // indirect
+	github.com/shomali11/proper v0.0.0-20180607004733-233a9a872c30 // indirect
+	github.com/shomali11/slacker v0.0.0-20220129203130-6c28a41fb7b0 // indirect
 	github.com/slack-go/slack v0.10.3 // indirect
 	github.com/sqweek/dialog v0.0.0-20220504154117-be45b268883a // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,66 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 
+	"github.com/shomali11/slacker"
 	"github.com/slack-go/slack"
 	"github.com/sqweek/dialog"
 )
 
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
 func main() {
 
-	os.Setenv("SLACK_BOT_TOKEN", "---your-bot-user-auth-token---")
-	os.Setenv("CHANNEL_ID", "---your-channel-id---")
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3291137781171-3607791117671-GPIrB7hUmg35BTxfcjXTXrvk")
+	os.Setenv("CHANNEL_ID", "C03K041GQM6")
 
-	Upload("SLACK_BOT_TOKEN", "CHANNEL_ID")
+	// app token for age bot
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03J3P3RYDU-3624781024820-ff85d7e78faaa63249e1d89a3105fc69ad3100926b787d0af90b5f16a35dbed8")
+
+	// Upload("SLACK_BOT_TOKEN", "CHANNEL_ID")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("My year of birth is <year>", &slacker.CommandDefinition{
+
+		Description: "yob calculator",
+		Example:     "My yob is 2002",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				log.Fatal(err)
+			}
+			age := 2022 - yob
+			r := fmt.Sprintf("Age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func Upload(slack_bot_token string, channel_id string) {
